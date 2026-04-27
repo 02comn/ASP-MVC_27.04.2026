5d4766d25fbeda76f28e28d1ac16ff1266f5788b
leonardojsantoos <pedro.m.fonseca@aluno.senai.br>
2026-04-27 15:50:52 -0300

    Publish
    ASP MVC Layout

---
+++ Atividade/idontknowwhattoputherehonestly/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace idontknowwhattoputherehonestly.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
