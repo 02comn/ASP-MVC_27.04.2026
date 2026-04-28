fc19f4ad0a8bebca7f6a5164605e494aafe14aa4
Cicero Miguel <pedro.m.fonseca@aluno.senai.br>
2026-04-28 12:23:48 -0300

    Cidade Controller e View
    "Estância eh! Estância eh! Estância ah!" wawa

---
+++ Atividade/idontknowwhattoputherehonestly/Controllers/CidadeController.cs
@@ -0,0 +1,18 @@
+﻿using AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace idontknowwhattoputherehonestly.Controllers
+{
+    public class CidadeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detalhes()
+        {
+            return View();
+        }
+    }
+}
